b8ada9c5dfe1e4f0505cdfbd8489a5efd2829992
lby <2283165894@qq.com>
2023-11-25 11:03:58 +0800

    短链接初始工程

---
+++ admin/src/main/java/com/lby/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,14 @@
+package com.lby.shortlink.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lby
+ */
+@SpringBootApplication
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
